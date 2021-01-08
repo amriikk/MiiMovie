900ec7df898f516797e44a1127eda8b5cdaf1755
John Trujillo <amriikk@gmail.com>
2021-01-07 22:31:36 -0800

    update parameters for map function in MovieList component

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -4,8 +4,8 @@ const MovieList = (props) => {
     return (
         <>
             {
-                props.movies.map( () => (
-                    <div>
+                props.movies.map( (movie, index) => (
+                    <div className='image-container d-flex justify-content-start m-3'>
                         <img src={movie.Poster} alt='movie'></img>
                     </div>
                 ))
