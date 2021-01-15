f057d2120436afbeb7080df44baf2f29d6e380f7
John Trujillo <amriikk@gmail.com>
2021-01-14 19:59:29 -0800

    update + fix all typo bugs

--- src/App.js
+++ src/App.js
@@ -9,38 +9,40 @@ import './App.css';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
   const [favorites, setFavorites] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-  const getMovieRequest = async () => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
     
     const response = await fetch(url);
     const responseJson = await response.json();
-
+    
     if (responseJson.Search) {
       setMovies(responseJson.Search);
     }
   };
-
+  
   useEffect( () => {
     getMovieRequest(searchValue);
-  }, [searchValue]);
+  }, [searchValue]);  
 
   useEffect( () => {
     const movieFavorites = JSON.parse(
       localStorage.getItem('react-movie-app-favorites')
-    );
+    );  
 
-    setFavorites(movieFavorites);
-  }, []);
+    if (movieFavorites) {
+      setFavorites(movieFavorites);
+    }
+  }, []);  
 
   const saveToLocalStorage = (items) => {
     localStorage.setItem('react-movie-app-favorites', JSON.stringify(items));
-  };
+  };  
   
   const AddFavoriteMovie = (movie) => {
-		const newFavoriteList = [...favorites, movie];
+    const newFavoriteList = [...favorites, movie];
     setFavorites(newFavoriteList);
     saveToLocalStorage(newFavoriteList);
   };
@@ -64,8 +66,8 @@ const App = () => {
       <div className='row'>
         <MovieList 
           movies={movies} 
-          favoriteComponent={AddFavorite}
           handleFavoritesClick={AddFavoriteMovie} 
+          favoriteComponent={AddFavorite}
         />
       </div>
       <div className='row d-flex align-items-center mt-4 mb-4'>
